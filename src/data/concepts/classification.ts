9659dc2fcd83c977452b5fb23b99e1a01569c952
Jeroen <jvos@infiniot.nl>
2025-12-29 13:45:36 +0100

    Updates

--- src/data/concepts/classification.ts
+++
@@ -1,7 +0,0 @@
-import { logisticRegression } from './logistic-regression';
-import { naiveBayes } from './naive-bayes';
-import { decisionTreeClassifier } from './decision-tree-classifier';
-import { svm } from './svm';
-import { knn } from './knn-classifier';
-
-export { logisticRegression, naiveBayes, decisionTreeClassifier, svm, knn };
--- src/data/concepts/regression.ts
+++
@@ -1,7 +0,0 @@
-import { polynomialRegression } from './polynomial-regression';
-import { linearRegression } from './linear-regression';
-import { ridgeLasso } from './ridge-lasso';
-import { decisionTreeRegressor } from './decision-tree-regressor';
-import { knnRegressor } from './knn-regressor';
-
-export { polynomialRegression, linearRegression, ridgeLasso, decisionTreeRegressor, knnRegressor };
